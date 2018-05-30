c3221c410c904dcc1fde468894b85887677f3f86
StormFireFox1 <matei@gardus.eu>
2018-05-30 10:18:23 +0300

    Adaugare functie updateServer

--- APIAdmin.go
+++ APIAdmin.go
@@ -28,6 +28,11 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"io/ioutil"
+	"github.com/buger/jsonparser"
+	"strings"
+	"runtime"
+	update "github.com/inconshreveable/go-update"
 )
 
 // downloadLogs will download all of the logs currently present in the "log" folder.
@@ -43,7 +48,7 @@ func downloadLogs(w http.ResponseWriter, r *http.Request) {
 	if !authOK || (username != user || password != pass) {
 		responseCode = http.StatusUnauthorized
 		w.WriteHeader(responseCode)
-		w.Header().Set("WWW-Authenticate", `Basic realm="127.0.0.1"`)
+		w.Header().Set("WWW-Authenticate", `Basic realm="Access to the admin section"`)
 		fmt.Fprint(w, "Invalid authentication scheme!")
 		return
 	}
@@ -68,6 +73,87 @@ func downloadLogs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/zip")
 	http.ServeFile(w, r, "log/VianuEdu_Server_Logs-"+strconv.Itoa(year)+"-"+month.String()+"-"+strconv.Itoa(day)+".zip")
 	os.Remove("log/VianuEdu_Server_Logs-" + strconv.Itoa(year) + "-" + month.String() + "-" + strconv.Itoa(day) + ".zip")
+}
+
+// updateServer calls for the server to download the latest binary from the latest release of this server's GitHub
+// repository.
+//
+// The method calls the GitHub API and downloads the latest release whose binary matches this program's name (for OS
+// compatibility) and self-updates. After that, a manual restart is required for the server to apply its changes.
+func updateServer(w http.ResponseWriter, r *http.Request) {
+
+	username, password, authOK := r.BasicAuth()
+	responseCode := http.StatusOK
+
+	user, pass := GetAdminCreds()
+
+	if !authOK || (username != user || password != pass) {
+		responseCode = http.StatusUnauthorized
+		w.Header().Set("WWW-Authenticate", `Basic realm="Access to the admin section"`)
+		http.Error(w, "Invalid authentication scheme!", responseCode)
+		return
+	}
+
+	resp, err := http.Get("https://api.github.com/repos/CNITV/VianuEdu-Server/releases/latest")
+	if err != nil {
+		responseCode = http.StatusInternalServerError
+		w.WriteHeader(responseCode)
+		fmt.Fprintf(w, "Could not get API URL for update! (%s)", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	downloadURLJSON, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		responseCode = http.StatusInternalServerError
+		w.WriteHeader(responseCode)
+		fmt.Fprintf(w, "Could not extract body from HTTP request! (%s)", err.Error())
+		return
+	}
+
+	updateURL := ""
+	_, err = jsonparser.ArrayEach(downloadURLJSON, func(value []byte, dataType jsonparser.ValueType, offset int, err1 error) {
+		asset, err2 := jsonparser.GetString(value, "browser_download_url")
+		if err2 != nil {
+			return
+		}
+
+		if runtime.GOOS == "windows" {
+			if strings.Contains(asset, ".exe") {
+				updateURL = asset
+			}
+		} else {
+			if !strings.Contains(asset, ".exe") {
+				updateURL = asset
+			}
+		}
+	}, "assets")
+	if err != nil {
+		responseCode = http.StatusInternalServerError
+		w.WriteHeader(responseCode)
+		fmt.Fprintf(w, "Could not get asset for update from JSON array! (%s)", err.Error())
+		return
+	}
+
+	updateResp, err := http.Get(updateURL)
+	if err != nil {
+		responseCode = http.StatusInternalServerError
+		w.WriteHeader(responseCode)
+		fmt.Fprintf(w, "Could not update from URL! (%s)", err.Error())
+		return
+	}
+	defer updateResp.Body.Close()
+
+	err = update.Apply(updateResp.Body, update.Options{})
+	if err != nil {
+		responseCode = http.StatusInternalServerError
+		w.WriteHeader(responseCode)
+		fmt.Fprintf(w, "Could not apply update! (%s)", err.Error())
+		return
+	}
+
+	fmt.Fprint(w, "Update succesful! Restart the server!")
+	HTTPLogger.Warn("[WARN] Server has been updated through updateServer HTTP Handler! Restart!")
 }
 
 // ZipFiles creates a ZIP archive by receiving the filepath to each of the respective files.
